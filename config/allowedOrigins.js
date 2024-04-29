0bb456df5c397e410fdf1f94ef18ea869c902b7e
AbeFisher <143359301+AbeFisher@users.noreply.github.com>
2024-04-29 16:58:21 -0500

    Updated allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    // 'https://www.yoursite.com',
+    'https://tattered-panoramic-patch.glitch.me/states',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000'
