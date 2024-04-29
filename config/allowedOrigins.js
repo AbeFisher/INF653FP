59c0e46cbda498b13a389a467abf309cc42eab28
AbeFisher <143359301+AbeFisher@users.noreply.github.com>
2024-04-29 17:18:43 -0500

    Updates to work with Glitch

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'https://tattered-panoramic-patch.glitch.me/states',
+    'https://dazzling-snickerdoodle-777101.netlify.app/',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000'
